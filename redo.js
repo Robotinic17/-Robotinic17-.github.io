212fe37055b29a79de4d5ed7d07f27549c433f21
Dave <davidtimileyin55@gmail.com>
2024-11-12 11:24:30 +0100

    Add files via upload

---
+++ redo.js
@@ -0,0 +1,28 @@
+
+const nav = document.getElementById("menu");
+nav.addEventListener('click', toggleNav);
+nav.addEventListener('mouseover', toolTip);
+nav.addEventListener('mouseout', ntoolTip );
+
+function toggleNav() {
+   const sideNav = document.getElementById('side-nav');
+   sideNav.style.display = 'block'; // Show the element
+   sideNav.style.left = '266px';
+}
+
+
+function toolTip(){
+    document.getElementById('menutooltip').style.display = 'inline';
+}
+
+function ntoolTip(){
+    document.getElementById('menutooltip').style.display = 'none';
+}
+
+
+ const navcancel = document.getElementById("cancel");
+ navcancel.addEventListener('click', navcan);
+
+ function navcan(){
+     document.getElementById('side-nav').style.display = 'none';
+ }
